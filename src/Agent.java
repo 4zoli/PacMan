88e878c6c62d8c0ffc4904fc54fee277673c3e56
Oli <h.oliver9112@gmail.com>
2019-11-07 21:42:23 +0100

    no message

--- src/Agent.java
+++ src/Agent.java
@@ -1,38 +1,32 @@
 import game.pm.strategies.Strategy;
+import javafx.geometry.Pos;
+
+import javax.xml.ws.FaultAction;
+
+import game.engine.utils.Pair;
 import game.pm.PMAction;
 import game.pm.PMGame;
-import game.pm.PMGame.TILES;
-import game.pm.PMPlayer;
 
 public class Agent extends Strategy {
-	public int faultCounter = 0;
+	public int whereToGo = 0;
 
 	@Override
 	public int getDirection(int id, PMGame game) {
 		int currentDirection = game.pacmans[id].getDirection();
-		int maxScore = -1;
-		int maxDirection = -1;
-		for (int direction = 0; direction < 4; direction++) {
-			PMGame cloned = game.clone();
-			for (int j = 0; j < PMGame.TILES.SIZE / cloned.pacmans[id].getSpeed() + 1; j++) {
-				cloned.setAction(cloned.pacmans[id], new PMAction(direction), 0);
-			}
-			
 
-			
-			if (maxScore < cloned.score) {
-				maxScore = cloned.score;
-				maxDirection = direction;
+		if (!game.pacmans[id].canMoveTo(currentDirection)) {
+			for(int direction = 0; direction < 4; direction++)	{
+				if(game.pacmans[id].canTurnAndMove(direction)) {
+					Pair<Integer, Integer> pacmansTilePosition = game.pacmans[id].getTilePosition();
+					
+					int i = game.getTileAt(pacmansTilePosition.first, pacmansTilePosition.second);
+						
+					
+					return direction;
+				}
 			}
-			
 		}
-		
-		if(!game.pacmans[0].canTurnAndMove(currentDirection) && maxDirection == 0) {
-			return random.nextInt(4);
-		}
-
-		return maxDirection;
+		return currentDirection;
 
 	}
-
 }
